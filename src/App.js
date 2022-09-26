801e2d660abc9894822f0385ddd6b431846512e8
AntonShalukho <antonshalukho@gmail.com>
2022-09-26 10:49:16 +0200

    clear branch for new homework

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import { Component } from 'react';
-import Form from './components/form/Form';
 
 class App extends Component {
 
   render() {
     return (
       <div className='App'>
-        <Form/>
       </div>
     )
   }
